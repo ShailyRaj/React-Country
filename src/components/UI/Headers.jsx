788f4cb369abdeabc598e4f3438cb7f2ca4d2e44
Shaily <rajshaily807@gmail.com>
2024-12-28 19:30:35 +0530

    hero-navbar-part

--- src/components/UI/Headers.jsx
+++ src/components/UI/Headers.jsx
@@ -1,7 +1,45 @@
+import {NavLink} from 'react-router-dom';
+
 export const Headers = () => {
     return (
-        <>
-            <h1>Header page</h1>
-        </>
+        <header>
+            <div className="container">
+                <div className="grid navbar-grid">
+
+                    <div className="Logo">
+                        <NavLink to="/">
+                            <h1>ExploreWorld</h1>
+                        </NavLink>
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to = "/">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to = "/about">
+                                    About
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to = "/country">
+                                    Country
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to = "/contact">
+                                    Contact
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+
+                </div>
+            </div>
+
+        </header>
     )
 }
